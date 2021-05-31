78966b31deaed15a35a754fa5c900908656f766c
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-05-31 12:56:44 +0200

    added submit style

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import userReducer from './userReducer';
 
 const initialState = {
   name: "",
@@ -11,11 +10,7 @@ const initialState = {
 }
 
 const reducers = combineReducers({
-  // ...your other reducers here
-  // you have to pass formReducer under 'form' key,
-  // for custom keys look up the docs for 'getFormState'
-  form: formReducer,
-  addUser : userReducer
+  form: formReducer
 })
 
 const store = createStore(
